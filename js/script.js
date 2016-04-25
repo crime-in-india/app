fe289d5762bdce6f38e6980a2173cc6f9d5743b1
Saurabh Datar <sdatar2@stanford.edu>
2016-04-24 21:42:39 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready( function () {
+    var map = L.map('map').setView([21, 78], 4);
+
+    var mapquestLayer = new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
+          {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+            maxZoom: 18
+          });
+    map.addLayer(mapquestLayer);
+});
