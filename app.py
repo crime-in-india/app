9c9b3e4585697117a3a83357d9aea52d41e44e75
Saurabh Datar <sdatar2@stanford.edu>
2016-05-12 02:41:13 -0700

    Added landing pages for crime and data dump

--- app.py
+++ app.py
@@ -19,13 +19,32 @@ def homepage():
 def city_data(city):
   template = 'city-page.html'
   temp = get_data()
-  data=[]
+  data = []
 
   for row in temp:
     if row['city'] == city:
       data.append(row)
   return render_template(template, data=data, city=city, total=temp)
 
+@app.route("/crime/<crime>/")
+def crime_data(crime):
+  template = 'crime-page.html'
+  temp = get_data()
+  data = []
+
+  data = [row for row in temp if row['crime_name'] == crime]
+  return render_template(template, crime_data=data, total=temp, crime=crime)
+
+@app.route("/crime-landing")
+def crime_landing():
+  template = 'crime-landing.html'
+  return render_template(template)
+
+@app.route("/data")
+def data_table():
+  template = 'data.html'
+  data = get_data()
+  return render_template(template, master=data)
+
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
-
