bae5bc1b7e12f933a6e9484787060d29e0dc4123
Saurabh Datar <sdatar2@stanford.edu>
2016-05-23 16:21:31 -0700

    Added dowry graph

--- static/script.js
+++ static/script.js
@@ -70,14 +70,14 @@ function zoomed() {
 }
 
 $('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    var target = this.hash;
-    var $target = $(target);
+  var target = this.hash;
+  var $target = $(target);
 
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 900, 'swing', function () {
-        window.location.hash = target;
-    });
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 900, 'swing', function () {
+      window.location.hash = target;
+  });
 });
