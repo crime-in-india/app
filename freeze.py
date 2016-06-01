6e18c62b64cb955636d6a78ea5ff4409361f02b9
Saurabh Datar <sdatar2@stanford.edu>
2016-06-01 03:21:28 -0700

    Updated

--- freeze.py
+++ freeze.py
@@ -11,72 +11,33 @@ To deploy app:
 5. Site should automatically update.
 """
 
+import csv
+from os.path import join
+from operator import itemgetter
 from flask_frozen import Freezer
 from app import app
+
 freezer = Freezer(app)
 
 app.config['FREEZER_IGNORE_404_NOT_FOUND'] = True
-# @freezer.register_generator
-# def city_landing():
 
+@freezer.register_generator
+def city_page():
+    DATADIR = 'static/data'
+    fpath = join(DATADIR, 'total-final-2.csv')
+
+    with open(fpath, 'r') as csvin:
+        data = list(csv.DictReader(csvin))
+    for row in data:
+        yield { 'city': row['city'] }
+
+
+@freezer.register_generator
+def crime_page():
+  with open('static/data/total-final-2.csv', 'r') as csvin:
+    datarows = list(csv.DictReader(csvin))
+  for row in datarows:
+    yield { 'crime': row['crime_name'] }
 
 if __name__ == '__main__':
     freezer.freeze()
-
-# @app.route("/city-list")
-# def city_page(city):
-#   DATADIR = 'static/data/city-avg'
-#   citypath = join(DATADIR, city + '.csv')
-
-#################################################################
-
-
-# def crime_page(crime):
-  # template = 'crime-page.html'
-  # with open('static/data/total-final-2.csv', 'r') as csvin:
-  #   datarows = list(csv.DictReader(csvin))
-  # temp = get_data()
-  # data = []
-
-  # data = [row for row in datarows if row['crime_name'] == crime]
-  # return render_template(template, crime_data=data, total=temp, crime=crime)
-
-
-
-
-# def violent_landing_page():
-#     pass
-
-# def women_landing_page():
-#     pass
-
-
-# #################################################################
-
-# def prop_landing_page():
-#     pass
-
-
-
-# def econ_landing_page():
-#     pass
-
-# def about():
-#     pass
-
-# @app.route("/data")
-# def data_table():
-#     pass
-
-#################################################################
-
-# # Route to the story page
-# def story_page():
-#     pass
-
-
-# #################################################################
-
-# def ref_page():
-#     pass
-
