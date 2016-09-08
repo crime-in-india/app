1faec830c06f1e7814810d7e249839b5f81e53b7
Saurabh Datar <sdatar2@stanford.edu>
2016-09-07 23:34:08 -0400

    Added sitemap

--- build.py
+++ build.py
@@ -1,3 +1,12 @@
+'''
+This script is used to deploy the app in the following steps.
+1. Delete the old build directory
+2. Convert the app into static files
+3. Make a new build
+4. Copy the build into the crime-in-india directory for live hosting.
+
+'''
+
 import freeze, os, shutil
 from distutils.dir_util import copy_tree
 
